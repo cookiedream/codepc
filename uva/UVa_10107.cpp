74e11a708dee38a64a6bfb303a9439d1e2a6a8b2
Yen Yu <cookie2000891109@gamil.com>
2023-08-27 17:49:00 +0800

    UVa_10107

---
+++ uva/UVa_10107.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <algorithm>
+using namespace std;  
+
+int main()
+{
+    int n = 1;        // 設定整數 n 為 1，用來表示數組中的元素個數
+    int a[10001];     // 創建一個大小為 10001 的整數數組 a
+
+    // 使用 while 迴圈不斷讀取輸入，直到無法讀取為止
+    while (scanf("%d", &a[n]) == 1) {
+        sort(a, a + n + 1);    // 對數組 a 中的元素進行排序，排序範圍是從 a[0] 到 a[n]
+
+        if (n == 1) {
+            printf("%d\n", a[1]);   // 如果數組中只有一個元素，則直接輸出該元素
+        } else if (n % 2 == 0) {
+            printf("%d\n", (a[n / 2] + a[(n / 2) + 1]) / 2);   // 如果數組元素個數是偶數，計算中位數並輸出
+        } else if (n % 2 != 0) {
+            printf("%d\n", a[(n / 2) + 1]);   // 如果數組元素個數是奇數，直接輸出中間位置的元素作為中位數
+        }
+        n++;   // 增加數組中的元素個數
+    }
+    return 0;   // 程式執行完畢，返回 0 表示成功
+}
